959280b5b8d99c9d943f0ce519413708c07de915
eshahas <764701627@nzse.ac.nz>
2023-02-18 18:21:58 +1300

    Assessement 2

--- AheraTaera_CMS/Login.Designer.cs
+++ AheraTaera_CMS/Login.Designer.cs
@@ -42,7 +42,6 @@ namespace AheraTaera_CMS
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -52,9 +51,10 @@ namespace AheraTaera_CMS
             this.exitButton.BackColor = System.Drawing.SystemColors.Control;
             this.exitButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.exitButton.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.exitButton.Location = new System.Drawing.Point(47, 675);
+            this.exitButton.Location = new System.Drawing.Point(56, 810);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(112, 34);
+            this.exitButton.Size = new System.Drawing.Size(134, 41);
             this.exitButton.TabIndex = 1;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = false;
@@ -64,27 +64,30 @@ namespace AheraTaera_CMS
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(47, 352);
+            this.label1.Location = new System.Drawing.Point(56, 422);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 25);
+            this.label1.Size = new System.Drawing.Size(66, 30);
             this.label1.TabIndex = 2;
             this.label1.Text = "Email";
             // 
             // userTextBox
             // 
             this.userTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.userTextBox.Location = new System.Drawing.Point(163, 346);
+            this.userTextBox.Location = new System.Drawing.Point(196, 415);
+            this.userTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userTextBox.Name = "userTextBox";
-            this.userTextBox.Size = new System.Drawing.Size(210, 34);
+            this.userTextBox.Size = new System.Drawing.Size(251, 39);
             this.userTextBox.TabIndex = 3;
             // 
             // passwordTextBox
             // 
             this.passwordTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.passwordTextBox.Location = new System.Drawing.Point(163, 414);
+            this.passwordTextBox.Location = new System.Drawing.Point(196, 497);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(210, 34);
+            this.passwordTextBox.Size = new System.Drawing.Size(251, 39);
             this.passwordTextBox.TabIndex = 4;
             this.passwordTextBox.UseSystemPasswordChar = true;
             // 
@@ -92,9 +95,10 @@ namespace AheraTaera_CMS
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(47, 414);
+            this.label2.Location = new System.Drawing.Point(56, 497);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.Size = new System.Drawing.Size(105, 30);
             this.label2.TabIndex = 5;
             this.label2.Text = "Password";
             // 
@@ -103,9 +107,10 @@ namespace AheraTaera_CMS
             this.loginBotton.BackColor = System.Drawing.Color.DodgerBlue;
             this.loginBotton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.loginBotton.ForeColor = System.Drawing.SystemColors.Control;
-            this.loginBotton.Location = new System.Drawing.Point(47, 500);
+            this.loginBotton.Location = new System.Drawing.Point(56, 600);
+            this.loginBotton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loginBotton.Name = "loginBotton";
-            this.loginBotton.Size = new System.Drawing.Size(233, 47);
+            this.loginBotton.Size = new System.Drawing.Size(280, 56);
             this.loginBotton.TabIndex = 7;
             this.loginBotton.Text = "Login";
             this.loginBotton.UseVisualStyleBackColor = false;
@@ -115,9 +120,10 @@ namespace AheraTaera_CMS
             // 
             this.profileButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.profileButton.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.profileButton.Location = new System.Drawing.Point(47, 565);
+            this.profileButton.Location = new System.Drawing.Point(56, 678);
+            this.profileButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.profileButton.Name = "profileButton";
-            this.profileButton.Size = new System.Drawing.Size(233, 47);
+            this.profileButton.Size = new System.Drawing.Size(280, 56);
             this.profileButton.TabIndex = 8;
             this.profileButton.Text = "Create  an Account";
             this.profileButton.UseVisualStyleBackColor = true;
@@ -127,9 +133,10 @@ namespace AheraTaera_CMS
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(450, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(540, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(567, 739);
+            this.pictureBox1.Size = new System.Drawing.Size(680, 887);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -137,9 +144,10 @@ namespace AheraTaera_CMS
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(47, 40);
+            this.pictureBox2.Location = new System.Drawing.Point(56, 48);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(180, 163);
+            this.pictureBox2.Size = new System.Drawing.Size(216, 196);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
@@ -148,38 +156,29 @@ namespace AheraTaera_CMS
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(47, 229);
+            this.label3.Location = new System.Drawing.Point(56, 275);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(173, 38);
+            this.label3.Size = new System.Drawing.Size(204, 45);
             this.label3.TabIndex = 11;
             this.label3.Text = "Ahera Taera";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(227, 700);
+            this.label4.Location = new System.Drawing.Point(272, 840);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(195, 25);
+            this.label4.Size = new System.Drawing.Size(222, 30);
             this.label4.TabIndex = 12;
             this.label4.Text = "Ver 1.0 Copyright 2023";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(261, 91);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "Admin";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1015, 734);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1218, 881);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox2);
@@ -192,6 +191,7 @@ namespace AheraTaera_CMS
             this.Controls.Add(this.label1);
             this.Controls.Add(this.exitButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MinimizeBox = false;
             this.Name = "Login";
             this.Text = "Login";
@@ -216,6 +216,5 @@ namespace AheraTaera_CMS
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
     }
 }
